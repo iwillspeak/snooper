79a31c956638cade4ab63b690a3c2a1e8308f004
Will Speak <lithiumflame@gmail.com>
2013-07-07 12:05:50 +0100

    Bump Version

--- lib/snooper/version.rb
+++ lib/snooper/version.rb
@@ -10,5 +10,5 @@ module Snooper
   #
   # This should conform to SemVer. If this is changed it should be the only
   # thing that changes in the comit.
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 end
